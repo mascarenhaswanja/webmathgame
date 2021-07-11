7723fdde014bde8a7dff0752b55fd2429c30943a
mayankaryaca <mayankaryaca@gmail.com>
2021-07-11 12:47:36 -0400

    new chnages

--- js/game.js
+++ js/game.js
@@ -5,24 +5,18 @@ console.log("Output - Expert Math Frog");
 
 let player
 // let question = []
-let score = 0
-let fly = []
-const x = document.body.clientWidth;
-const y = document.body.clientHeight;
 
-
-
-const startGame = () => {
-    console.log("Start Game");
-    /* @TODO: In each level, enemies move from left to right at random speeds.
-     Some enemies will move faster; others will move slower. */
-    for (let i = 0; i < 5; i++) {
-        fly.push(new Fly(10))
-    }
-
-    // question = generateQuestions()
-}
-
+let level = 1; 
+let timeleft = 60; 
+let trigger ;
+let timerVar ;
+let gameContinueCheck ;
+let itemColGameEnd = 0;
+let currentFrogRow = 3;
+let answerArray = new Array();
+let questionArray = new Array();
+let currentAnswerLocation = 0;
+let answerSetUp = [2,5,1,4,3];
 
 /*  For each row generate the question with random numbers
 Each multiplication question must consist of two randomly generated numbers.
@@ -33,47 +27,38 @@ const generateQuestion= () => {
         // generate different questions for each level
 
         let question = ""
-            const first = Math.floor((Math.random() * 15) + 1)
-            const second = Math.floor((Math.random() * 10) + 1)
-            question = `${parseInt(first)} x ${parseInt(second)} = ?`
+            const first = parseInt(Math.floor((Math.random() * 15) + 1));
+            const second = parseInt(Math.floor((Math.random() * 10) + 1));
+            
+            question = `${first} x ${second} = ?`;
+            questionArray.push(question)
+            answerArray.push(first*second);
     
     return question
 }
-
-class Fly {
-    constructor(speed) {
-      this.speed = speed
-      this.randomSpeed()
-    }
-  
-    randomSpeed() {
-      this.speed = Math.floor(Math.random() * 10) + this.speed / 10
-    }
-}
-
-
-startGame()
-
-
-
 /// new code -- mayank
 
-const level = 0; 
-let timeleft = 60; 
-var trigger ;
-let itemColGameEnd = 0
+
 
 const initalSetup = () => {
+  for(i = 0; i<5 ; i++){
+    generateQuestion();
+  }
+  console.log("Answer ",answerArray);
+  console.log("Questn ",questionArray);
   document.querySelector(".game").innerHTML = `
-  <img  class="img-frog" src="assets/img/frog_new.png">
+  <div id="frog">
+  <img class="img-frog" src="assets/img/frog_new.png">
+  <p class="frog-answer-box">${answerArray[currentAnswerLocation]}</p>
+  </div>
   `
-  document.querySelector(".img-frog").style.gridColumn = '20';
-  document.querySelector(".img-frog").style.gridRow = '3';  
+  document.querySelector("#frog").style.gridColumn = '20';
+  document.querySelector("#frog").style.gridRow = '3';  
   for(let i = 0; i< 5; i++){
     document.querySelector(".game").innerHTML += `
-  <div class="location-col-1 " id="fly-${i+1}">
+  <div class="location-col-1" id="fly-${i+1}">
   <img   class="img-fly" src="assets/img/fly.png">
-  <p class="fly-question-box">${generateQuestion()}</p>
+  <p class="fly-question-box">${questionArray[i]}</p>
   </div>
   `
   document.querySelector(`#fly-${i+1}`).style.gridColumn = '1'
@@ -81,16 +66,22 @@ const initalSetup = () => {
   }
 
   
+};
+
+const setFrogAnswer = () => {
+  document.querySelector(".frog-answer-box").innerText = answerArray[currentAnswerLocation]
 }
 const gameOver = () => {
-  if(itemColGameEnd === 20 || timeleft === 0 || level >= 5){
+  if(itemColGameEnd >= 20 || timeleft === 0 || level > 5){
      //game over
       console.log("Game over");
       clearInterval(trigger)
+      clearInterval(timerVar);
+   
     }else{
     return;
   }
-}
+};
 const changeFlyLocation = (col) => {
     let selectedFly = Math.floor(Math.random() * 5) + 1;
     let currentFlyCol = document.querySelector(`#fly-${selectedFly}`).style.gridColumn
@@ -98,38 +89,30 @@ const changeFlyLocation = (col) => {
     let newFlyCol = parseInt(currentFlyCol) + col ;
     if(newFlyCol >= itemColGameEnd){
       itemColGameEnd = newFlyCol;
-      console.log(itemColGameEnd);
       gameOver();
     }
     if(newFlyCol <= 20){
-      console.log("Fly col" ,newFlyCol)
       document.querySelector(`#fly-${selectedFly}`).style.gridColumn = `${newFlyCol}`;
       document.querySelector(`#fly-${selectedFly}`).style.gridRowStart = `${selectedFly}`;
     }
 
-}
+};
 const changeFrogLocation = (row) => {
-  document.querySelector(".img-frog").style.gridRow = row;
-}
-const randomSpeed = () => {
-  const speed = Math.floor(Math.random() * 4) + 1;
-  return speed;
+  document.querySelector("#frog").style.gridRow = row;
+};
 
-}
-const gameFlyLocation = () => {
-    let col = randomSpeed();
+const animateFly = () => {
+    let col = Math.floor(Math.random() * 4) + 1;
     changeFlyLocation(col)
-}
+};
 
-const gameLogic = () => {
-  timer();
+const startGame = () => {
+  questionArray = [];
+  answerArray = [];
+  currentAnswerLocation = 0;
   initalSetup();
-  gameFlyLocation();
-  trigger = setInterval(gameFlyLocation,1000);
-}
-
-
-let currentFrogRow = 3;
+  trigger = setInterval(animateFly,1000);
+};
   document.onkeydown = function (event) {
   
       switch (event.keyCode) {
@@ -157,49 +140,62 @@ let currentFrogRow = 3;
         case 32:
           //console.log("Space bar pressed");
           checkAnswer();
+          setFrogAnswer();
+          checkIfLevelCleared();
           break;
      }
     
    
  };
 const checkAnswer = () => {
-  let frogRow = document.querySelector(".img-frog").style.gridRow
+  let frogRow = document.querySelector("#frog").style.gridRow
+  console.log(document.querySelector(".frog-answer-box").innerText)
   let flyRow = document.querySelector(`#fly-${frogRow.charAt(0)}`)
-  let question = flyRow.querySelector(".fly-question-box").innerText
+  let answerForFly = answerArray[frogRow.charAt(0) - 1];
+  console.log("answer for fly", answerForFly);
+  console.log("answer", answerArray[currentAnswerLocation]);
+
+  if(answerForFly === answerArray[currentAnswerLocation]){
+    console.log("Correct answer");
+    currentAnswerLocation += 1;
+    setFrogAnswer();
+
+  }else{
+    console.log("incorrect answer");
+  }
+  console.log(answerForFly);
+
   removeFlyOnCorrectAnswer(frogRow.charAt(0));
-  console.log(question);
-}
+};
 const timer = () => {
-  var downloadTimer = setInterval(function(){
+  timerVar = setInterval(function(){
   if(timeleft <= 0){
-    clearInterval(downloadTimer);
+    clearInterval(timerVar);
   }else{
     timeleft -= 1;
   }
   document.querySelector("#remaining").innerText = timeleft;
 }, 1000);
-}
-
-// gameLogic();
-
+};
 const removeFlyOnCorrectAnswer = (fly_id) => {
   document.querySelector(`#fly-${fly_id}`).innerHTML = ""
-}
+};
 
-const setLevel = (level) => {
-  document.querySelector("#level").innerText = level;
+const checkIfLevelCleared = () => {
   let checkIfFlyExist = document.querySelector(".img-fly")
-  if(checkIfFlyExist == ""){
-     if(level >= 5){
-       gameOver();
-     }else{
-       
-     }
-  }else{
+  if(checkIfFlyExist == null && level <5 ){
+    startGame();
     level += 1;
-    gameLogic();
-    //need to fix running after level clearance
-    
+    document.querySelector("#level").innerText = level;
+
+  }else{
+
+    console.log("max level reached")
+    gameOver();
+    return;
   }
 }
-setLevel(1);
+
+timer();
+
+startGame();
